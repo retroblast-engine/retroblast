ac4b06c2f2b3337ae860b090d36d10ae4155ab3f
ppapageorge <panopapageorgiou@gmail.com>
2024-12-10 21:57:25 +0200

    Generate command gitRepoUrl Windows equivalent

--- cmd/git.go
+++ cmd/git.go
@@ -4,11 +4,16 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"runtime"
 	"strings"
 )
 
 // getRepoURL retrieves the repository URL from the Git configuration
 func getRepoURL() (string, error) {
+
+	var cmd *exec.Cmd
+	var err error
+	var output []byte
 
 	// Get the GOPATH from the environment variable
 	goPath := os.Getenv("GOPATH")
@@ -16,8 +21,19 @@ func getRepoURL() (string, error) {
 		return "", fmt.Errorf("GOPATH is not set")
 	}
 
-	cmd := exec.Command("sh", "-c", "pwd | awk -F "+goPath+" '{print $2}'")
-	output, err := cmd.Output()
+	// Check the operating system
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("powershell", "-Command", fmt.Sprintf("$pwd = Get-Location; $pwd.Path -split \"%s\" | Select-Object -Index 1", "/"))
+	} else if runtime.GOOS == "linux" {
+		cmd = exec.Command("sh", "-c", fmt.Sprintf("pwd | awk -F \"%s\" '{print $2}'", "/"))
+	} else {
+		fmt.Println("Unsupported OS")
+		return "", fmt.Errorf("unsupported OS")
+	}
+
+	output, err = cmd.Output()
+
+	fmt.Println(output)
 
 	if err != nil {
 		return "", err
